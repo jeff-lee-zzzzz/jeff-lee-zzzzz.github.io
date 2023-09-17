79d8fa455a420eee357bf0b060b061bb4e33c8e8
nobody <nowhere@on.the.net>
2023-09-17 21:44:56 +0900

    initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+Forked from https://github.com/ixartz/Next-js-Blog-Boilerplate
---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,9 @@
+export const AppConfig = {
+  site_name: "jeff-lee-zzzzz",
+  title: "Jeff Lee",
+  description: "Starting point.",
+  url: "https://jeff-lee-zzzzz.github.io",
+  locale: "ko",
+  author: "Anonymous",
+  pagination_size: 10,
+};
